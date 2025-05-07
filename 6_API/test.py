5f9b63cf49362652730096bb19b32b2ebcd0fbfa
Maksym Rubtsov <maksrub04@gmail.com>
2025-05-07 17:45:29 +0200

    API_bitcoin_price

--- 6_API/test.py
+++ 6_API/test.py
@@ -3,3 +3,22 @@ url = "https://api.coingecko.com/api/v3/coins/list"
 response = requests.get(url)
 coins_list = response.json()
 print("The first element of the coin list:", coins_list[8])
+
+
+
+#Obtaining current data on a specific coin
+coin_id = "bitcoin"
+url = f"https://api.coingecko.com/api/v3/coins/{coin_id}"
+params = {
+"localization": "false", # Отключить локализованные данные
+"tickers": "false", # Отключить тиканье (ticker data)
+"market_data": "true", # Включить рыночные данные
+"community_data": "false"
+,
+"developer_data": "false"
+,
+"sparkline": "false"
+}
+response = requests.get(url, params=params)
+bitcoin_data = response.json()
+print("Current price of Bitcoin:", bitcoin_data["market_data"]["current_price"]["usd"])
