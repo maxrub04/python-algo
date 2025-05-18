f9301dd454c6ead5e1f6ebbd043749a41dac7e68
Maksym Rubtsov <maksrub04@gmail.com>
2025-05-18 11:55:54 +0200

    tech_final

--- 7_Basic_Tech_Analysis/MACD.py
+++ 7_Basic_Tech_Analysis/MACD.py
@@ -10,3 +10,35 @@ df = pd.DataFrame({"Date": dates, "Price": prices})
 df.set_index("Date", inplace=True)
 
 
+# MACD = EMA(12) - EMA(26)
+df["EMA_12"] = df["Price"].ewm(span=12, adjust=False).mean()
+df["EMA_26"] = df["Price"].ewm(span=26, adjust=False).mean()
+df["MACD"] = df["EMA_12"] - df["EMA_26"]
+
+# Signal line MACD:
+df["Signal"] = df["MACD"].ewm(span=9, adjust=False).mean()
+df["Histogram"] = df["MACD"] - df["Signal"]
+plt.figure(figsize=(10, 6))
+
+
+# MACD and signal line chart
+plt.subplot(2, 1, 1)
+plt.plot(df.index, df["MACD"], label="MACD", color="blue")
+plt.plot(df.index, df["Signal"], label="Signal Line", color="orange")
+plt.title("MACD и signal line")
+plt.legend()
+plt.grid(True)
+plt.xticks(rotation=45)
+
+
+# Histogram of MACD and signal line difference
+plt.subplot(2, 1, 2)
+plt.bar(df.index, df["Histogram"], label="Histogram", color="gray")
+plt.title("Histogram MACD")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.legend()
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
