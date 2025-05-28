91e8d1594cc941f245cd71441198402233356004
Maksym Rubtsov <maksrub04@gmail.com>
2025-05-28 23:02:23 +0200

    classification_test

--- 10_Machine_Learning_In_Trading/classification.py
+++ 10_Machine_Learning_In_Trading/classification.py
@@ -6,3 +6,14 @@ X = data[["Price", "SMA_5"]]
 y = data["Signal"]
 # Divide data into training and tests sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and train a logistic regression model
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+# Prediction on the test set
+y_pred = clf.predict(X_test)
+# Estimating the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+print("Classification Report:")
+print(classification_report(y_test, y_pred))
