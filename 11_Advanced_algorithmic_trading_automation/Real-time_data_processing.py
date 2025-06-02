e65c76c12acbab622638d68ba51996f21cb18a66
Maksym Rubtsov <maksrub04@gmail.com>
2025-06-02 19:11:21 +0200

    data processing

---
+++ 11_Advanced_algorithmic_trading_automation/Real-time_data_processing.py
@@ -0,0 +1,29 @@
+import websocket
+import json
+import ssl
+
+def on_message(ws, message):
+    data = json.loads(message)
+    print("Receiving data:", data)
+
+def on_error(ws, error):
+    print("Error:", error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("Connection closed:")
+
+def on_open(ws):
+    print("Connection opened:")
+
+ws_url = "wss://stream.binance.com:9443/ws/btcusdt@trade"
+
+ws = websocket.WebSocketApp(
+    ws_url,
+    on_open=on_open,
+    on_message=on_message,
+    on_error=on_error,
+    on_close=on_close
+)
+
+# Run WebSocket with SSL verification turned off
+ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
