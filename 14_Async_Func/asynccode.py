47e3af270b57d4f7672f4a8e8b0cd9f4efa4efd9
Maksym Rubtsov <maksrub04@gmail.com>
2025-08-16 21:01:22 +0300

    asynccode

---
+++ 14_Async_Func/asynccode.py
@@ -0,0 +1,14 @@
+import asyncio
+
+async def fetch_data():
+    await asyncio.sleep(2) #  Immediate pause that does not block the event loop
+    return "Data fetched"
+async def main():
+    # Tasks are performed in parallel
+    task1 = asyncio.create_task(fetch_data())
+    task2 = asyncio.create_task(fetch_data())
+    result1 = await task1
+    result2 = await task2
+    print(result1, result2)
+
+asyncio.run(main())
