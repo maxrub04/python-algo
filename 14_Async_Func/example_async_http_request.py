f197e3009c03f690545c488a404fd91f7b15a083
Maksym Rubtsov <maksrub04@gmail.com>
2025-08-16 21:16:11 +0300

    async_http_request_update

--- 14_Async_Func/example_async_http_request.py
+++ 14_Async_Func/example_async_http_request.py
@@ -1,4 +1,4 @@
-import aiohttp #library for async work with HTTP
+"""import aiohttp #library for async work with HTTP
 import asyncio
 
 async def fetch_url(session, url):
@@ -16,5 +16,28 @@ async def main():
         responses = await asyncio.gather(*tasks)
         for url, content in zip(urls, responses):
             print(f"Content from {url} (first 100 symbols): {content[:100]}")
+
+asyncio.run(main())"""
+
+import asyncio
+import ssl
+import certifi
+from aiohttp import ClientSession, TCPConnector
+
+
+async def fetch_url(url: str):
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    connector = TCPConnector(ssl=ssl_context)
+
+    async with ClientSession(connector=connector) as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+
+async def main():
+    url = "https://example.com"
+    result = await fetch_url(url)
+    print(result[:200])  #
+
 
 asyncio.run(main())
